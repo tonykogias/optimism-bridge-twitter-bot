53ff2ff595a5bc8ab4b1ebaba8f57c0f36be4d54
Antonis Kogias <tonykogias@gmail.com>
2022-05-24 16:13:01 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,9 @@ module.exports = {
   apps : [{
     name: "Optimism Bridge Twitter Bot",
     script: "npx ts-node src/main.ts"
-  }]
+  }],
+
+  deploy : {
+    "post-deploy" : `pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`
+  }
 };
